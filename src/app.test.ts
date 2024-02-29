931894e64f19a017d6901657da1f796768eddbe5
Minghe <h.minghe@gmail.com>
2024-02-29 10:38:58 +0800

    release 0.0.8 to npm (#22)
    * chore(async): add test case for async resolve function (#15)
    
    * chore(deps): bump undici from 5.28.2 to 5.28.3 (#18)
    
    Bumps [undici](https://github.com/nodejs/undici) from 5.28.2 to 5.28.3.
    - [Release notes](https://github.com/nodejs/undici/releases)
    - [Commits](https://github.com/nodejs/undici/compare/v5.28.2...v5.28.3)
    
    ---
    updated-dependencies:
    - dependency-name: undici
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * feat(env): support cloudflare worker environment variable (#19)
    
    * feat(env): support cloudflare worker environment variable
    
    * add automatical release note generation
    
    * chore(cicd): enable auto release (#21)
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- src/app.test.ts
+++ src/app.test.ts
@@ -66,6 +66,44 @@ describe('App', () => {
     })
   })
 
+  it('should work when schema registered with async resolve function', async () => {
+    const app = new EdgeQL()
+    const queryType = new GraphQLObjectType({
+      name: 'Query',
+      fields: {
+        hello: {
+          type: GraphQLString,
+          resolve: async () => {
+            return new Promise((res) => {
+              res('world')
+            })
+          },
+        },
+      },
+    })
+    app.register(new GraphQLSchema({ query: queryType }))
+
+    const req = new Request('http://localhost', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: 'query H { hello }',
+        variables: {},
+        operationName: 'H',
+        extensions: {},
+      }),
+    })
+    const res = await app.fetch(req)
+    expect(res.status).toBe(200)
+    expect(await res.json()).toEqual({
+      data: {
+        hello: 'world',
+      },
+    })
+  })
+
   it('should work when schema string registered', async () => {
     const app = new EdgeQL()
     const schema = `
@@ -203,6 +241,37 @@ type Query {
       data: {
         hi: 'world',
         ping: 'pong',
+      },
+    })
+  })
+  it('should be able to get the environment variable inside handler', async () => {
+    const app = new EdgeQL()
+    const schema = `
+type Query {
+  hello: String
+}
+    `
+    app.register(schema, (parent, args, ctx) => {
+      return `${ctx.env.db} world`
+    })
+    const req = new Request('http://localhost', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: 'query H { hello }',
+        variables: {},
+        operationName: 'H',
+        extensions: {},
+      }),
+    })
+    const db = 'hello-db'
+    const res = await app.fetch(req, { db })
+    expect(res.status).toBe(200)
+    expect(await res.json()).toEqual({
+      data: {
+        hello: `${db} world`,
       },
     })
   })
