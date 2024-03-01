8c400dfaf3fea9c08de56cdae33766fe12cb2b4d
Minghe <h.minghe@gmail.com>
2024-03-01 14:06:53 +0800

    feat(statusText): support statusText setting (#24)

--- src/response.ts
+++ src/response.ts
@@ -1,11 +1,15 @@
 export class Res {
   private _headers: Headers = new Headers()
   private _status: number | undefined
+  private _statusText: string | undefined
   private _body: any
 
   toJSON(): Response {
+    this.headers.set('content-type', 'application/json')
+
     return new Response(JSON.stringify(this.body), {
       status: this.status ?? 200,
+      statusText: this.statusText ?? '',
       headers: this.headers,
     })
   }
@@ -20,8 +24,17 @@ export class Res {
   set status(status: number | undefined) {
     this._status = status
   }
+
   get status() {
     return this._status
+  }
+
+  set statusText(text: string | undefined) {
+    this._statusText = text
+  }
+
+  get statusText() {
+    return this._statusText
   }
 
   get headers() {
