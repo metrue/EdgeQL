cf9fe9c317bc7adc3c8acb0465762de57ea585d1
Minghe Huang <h.minghe@gmail.com>
2024-03-14 23:45:51 +0800

    feat(runtime): add runtime to ctx

--- src/context/runtime.ts
+++ src/context/runtime.ts
@@ -1,5 +1,7 @@
 import type { ExecutionContext, Environment } from '../types'
 
+type Runtime = 'node' | 'deno' | 'bun' | 'cloudflare' | 'fastly' | 'vercel' | 'lagon' | 'unknown'
+
 export class RuntimeContext {
   public readonly env?: Environment
   public readonly exeContext?: ExecutionContext
@@ -7,5 +9,39 @@ export class RuntimeContext {
   constructor(env?: Environment, exeContext?: ExecutionContext) {
     this.env = env
     this.exeContext = exeContext
+  }
+
+  get runtime(): Runtime {
+    const global = globalThis as any
+
+    if (global?.Deno !== undefined) {
+      return 'deno'
+    }
+
+    if (global?.Bun !== undefined) {
+      return 'bun'
+    }
+
+    if (typeof global?.WebSocketPair === 'function') {
+      return 'cloudflare'
+    }
+
+    if (global?.fastly !== undefined) {
+      return 'fastly'
+    }
+
+    if (typeof global?.EdgeRuntime === 'string') {
+      return 'vercel'
+    }
+
+    if (global?.process?.release?.name === 'node') {
+      return 'node'
+    }
+
+    if (global?.__lagon__ !== undefined) {
+      return 'lagon'
+    }
+
+    return 'unknown'
   }
 }
