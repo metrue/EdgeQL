f0aa79084776a63bddf20b01cb025e1d93069945
Minghe Huang <h.minghe@gmail.com>
2024-03-16 23:30:49 +0800

    fix(types): export types

--- examples/node/src/app.ts
+++ examples/node/src/app.ts
@@ -1,4 +1,5 @@
 import type { Context } from 'edgeql'
+import { cors } from 'edgeql/cors'
 import { NodeEdgeQL } from 'edgeql/node'
 
 const app = new NodeEdgeQL()
@@ -10,6 +11,8 @@ type Query {
 `, (ctx: Context) => {
   return  `hello from EdgeQL on ${ctx.runtime.runtime}`
 })
+
+app.use(cors())
 
 app.listen({port: 4000}, ({address, family, port}) => {
   console.log(address, family, port)
